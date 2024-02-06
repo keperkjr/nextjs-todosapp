4d71755ce1ecd29e0b1301ed1b3276894443872e
keperkjr <keperkjr@yahoo.com>
2024-02-05 19:23:23 -0800

    Added luckiest guy font, navbar

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,14 @@
-import { Inter } from "next/font/google";
+import { Inter, Luckiest_Guy } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
+
+const luckiestGuy = Luckiest_Guy({
+     subsets: ["latin"],
+     variable: "--font-luckiest-guy",
+     weight: "400"
+});
 
 export const metadata = {
   title: "Create Next App",
@@ -11,7 +18,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+        <body className={`${luckiestGuy.variable}`}>
+            <Navbar />
+            {children}
+        </body>
     </html>
   );
 }
