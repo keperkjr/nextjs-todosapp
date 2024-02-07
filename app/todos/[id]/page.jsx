5ecd937843acf9a71696d37bffe958fc7cbaabb8
keperkjr <keperkjr@yahoo.com>
2024-02-06 16:18:31 -0800

    Static routes

--- app/todos/[id]/page.jsx
+++ app/todos/[id]/page.jsx
@@ -1,5 +1,13 @@
 import React from 'react'
 
+export async function generateStaticParams() {
+    // const response = await fetch("https://example.com/movies.json");
+    // const data = await response.text();
+    // return data;
+
+    return [{id: "one"}, {id: "two"}, {id: "three"}];
+}
+
 function page({params}) {
   return (
     <div>Params ID: {params.id}</div>
