3b208661e82a43dcc607d80a301f8ea6d0986aa7
keperkjr <keperkjr@yahoo.com>
2024-02-07 18:38:43 -0800

    Data fetching

--- app/todos/page.jsx
+++ app/todos/page.jsx
@@ -1,9 +1,48 @@
 import React from 'react'
 
-function TodosPage() {
-  return (
-    <div>Todos Page</div>
-  )
+async function TodosPage() {
+    const response = await fetch("http://localhost:3000/api/todos", {
+        cache: "no-store", // dont cache the result
+        //next: { revalidate: 15 } // refresh cache period (seconds)
+    });
+    const data = await response.json();
+
+    console.log(data);
+
+    return (
+        <section className='mt-24 w-full h-full flex justify-center'>
+            <table className='min-w-max bg-white border border-gray-200 rounded-lg overflow-hidden'>
+                <thead className='bg-gray-100 text-gray-600 uppercase text-sm leading-normal'>
+                    <tr className='bg-gray-100 text-gray-600 uppercase text-sm leading-normal'>
+                        <th className='py-3 px-5 text-left'>
+                            Id
+                        </th>
+                        <th className='py-3 px-5 text-left'>
+                            Name
+                        </th>
+                        <th className='py-3 px-5 text-left'>
+                            Actions
+                        </th>                                                
+                    </tr>
+                </thead>
+                <tbody className='text-gray-600 text-sm font-light'>
+                    {data.todos.map(todo => (
+                        <tr key={todo.id} className='border-b border-gray-200 hover:bg-gray-100'>
+                            <td className='py-3 px-6'>
+                                {todo.id}
+                            </td>
+                            <td className='py-3 px-6'>
+                                {todo.name}
+                            </td>
+                            <td className='py-3 px-6'>
+                                <button>Delete</button>
+                            </td>                                                        
+                        </tr>    
+                    ))} 
+                </tbody>
+            </table>
+        </section>
+    )
 }
 
 export default TodosPage
